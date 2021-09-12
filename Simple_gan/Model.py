572ab3434ef4a847af70b81ced6441c08eaa8b92
YanisZatout <yanis.zatout@hotmail.com>
2021-09-12 17:27:40 +0200

    Corrected initiation for models

--- Simple_gan/Model.py
+++ Simple_gan/Model.py
@@ -3,6 +3,7 @@ from torch import nn
 
 class Generator(nn.Module):
     def __init__(self, noise_dim, img_dim):
+        super(Generator, self).__init__()
         self.gen = nn.Sequential(
             nn.Linear(noise_dim, 100),
             nn.ReLU(),
@@ -18,6 +19,7 @@ class Generator(nn.Module):
 
 class Discriminator(nn.Module):
     def __init__(self, img_dim):
+        super(Discriminator, self).__init__()
         self.disc = nn.Sequential(
             nn.Linear(img_dim, 100),
             nn.ReLU(),
